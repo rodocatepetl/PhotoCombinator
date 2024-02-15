a77f244115fbcd6d47448ca5056e81670ba73a54
Rodolfo Castillo <raindeerdeveloper+dev@gmail.com>
2024-02-15 15:27:45 -0600

    Initial Commit

---
+++ PhotoTelekinesis/PhotoTelekinesisApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoTelekinesisApp.swift
+//  PhotoTelekinesis
+//
+//  Created by Rodolfo Castillo on 15/02/24.
+//
+
+import SwiftUI
+
+@main
+struct PhotoTelekinesisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
